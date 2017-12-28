6d96ee7af5b573a157a686f753a814b52c9155aa
Nell <dongboxiang.nell@gmail.com>
2017-12-28 21:40:46 +0800

    an example

---
+++ 01_init_example.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets('/home/nell/work/hovel/DataBase/MNIST/raw/',
+                                  one_hot=True, reshape=True, validation_size=5000)
+
+x = tf.placeholder(tf.float32, [None, 784])
+
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+y = tf.nn.softmax(tf.matmul(x, W) + b)
+
+y_ = tf.placeholder(tf.float32, [None, 10])
+
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+#train
+#sess = tf.InteractiveSession()
+sess = tf.Session()
+#tf.global_variables_initializer().run()
+sess.run(tf.global_variables_initializer())
+for _ in range(1000):
+  batch_xs, batch_ys = mnist.train.next_batch(100)
+  sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+#eval
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+
+
+
